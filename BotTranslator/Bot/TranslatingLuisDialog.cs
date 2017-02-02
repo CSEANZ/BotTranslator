97c4ad757bfbfe0714c6c7e3b7098082fa635052
Jordan Knight <jakkaj@outlook.com>
2017-02-02 16:42:41 +1100

    Tuck away some more complexity in to the base class Ask if they would like transation

---
+++ BotTranslator/Bot/TranslatingLuisDialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BotTranslator.Dialog;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotTranslator.Bot
+{
+    [Serializable]
+    public class TranslatingLuisDialog : LuisDialog<object>, IDialog<object>
+    {
+        protected override async Task MessageReceived(IDialogContext context, IAwaitable<IMessageActivity> item)
+        {
+            if (!(context is TranslatingDialogContext))
+            {
+                context = new TranslatingDialogContext(context);
+            }
+
+            await base.MessageReceived(context, item);
+        }
+
+        async Task IDialog<object>.StartAsync(IDialogContext context)
+        {
+            var translatingContext = new TranslatingDialogContext(context);
+            await base.StartAsync(translatingContext);
+        }
+    }
+}
--- Samples/PizzaBot/PizzaOrderDialog.cs
+++ Samples/PizzaBot/PizzaOrderDialog.cs
@@ -11,39 +11,79 @@ using Newtonsoft.Json;
 using Microsoft.Bot.Builder.Luis.Models;
 using Microsoft.Bot.Connector;
 using BotTranslator;
+using BotTranslator.Services;
+
 namespace Microsoft.Bot.Sample.PizzaBot
 {
     [LuisModel("4311ccf1-5ed1-44fe-9f10-a6adbad05c14", "6d0966209c6e4f6b835ce34492f3e6d9", LuisApiVersion.V2)]
     [Serializable]
-    class PizzaOrderDialog : LuisDialog<object>, IDialog<object>
+    class PizzaOrderDialog : TranslatingLuisDialog
     {
-        internal PizzaOrderDialog()
-        {
-           
-        }
-
-        async Task IDialog<object>.StartAsync(IDialogContext context)
-        {
-            var translatingContext = new TranslatingDialogContext(context);
-            await base.StartAsync(translatingContext);
-        }
-
-        protected override async Task MessageReceived(IDialogContext context, IAwaitable<IMessageActivity> item)
-        {
-            if (!(context is TranslatingDialogContext))
-            {
-                context = new TranslatingDialogContext(context);
-            }
-
-            await base.MessageReceived(context, item);
-        }
-
 
         [LuisIntent("")]
         public async Task None(IDialogContext context, LuisResult result)
         {
+
+            //check the language - it may not be english!
+
+            if (TranslatorService.Instance.GetLanguage(context) != "en")
+            {
+                var checkLanguage = await TranslatorService.Instance.Detect(result.Query);
+                if (checkLanguage != "en")
+                {
+                    context.UserData.SetValue("checkLanguage", checkLanguage);
+
+                    EditablePromptDialog.Choice(context,
+                           LanuageSelectionChoicesAsync,
+                           new List<string> { "Yes", "No" },
+                           await TranslatorService.Instance.Translate("You are not speaking English! Would you like me to translate for you?", "en", checkLanguage),
+                           await TranslatorService.Instance.Translate("I didn't understand that. Please choose one of the options", "en", checkLanguage),
+                           2);
+
+                    return;}
+            }
+
             await context.PostAsync("I'm sorry. I didn't understand you.");
             context.Wait(MessageReceived);
+        }
+
+        public async Task LanuageSelectionChoicesAsync(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+
+                string choice = await result;
+
+                if (choice.ToLower() == "no")
+                {
+                    await context.PostAsync("No troubles, ignore me.");
+                }
+                else
+                {
+                    string checkLanguage;
+                    context.UserData.TryGetValue<string>("checkLanguage", out checkLanguage);
+                    if (string.IsNullOrWhiteSpace(checkLanguage))
+                    {
+                        await context.PostAsync("Something went wrong and I could not detect the language.");
+                    }
+                    else
+                    {
+                        TranslatorService.Instance.SetLanguage(context, checkLanguage);
+                        await context.PostAsync("No problem - it has been set!");
+                    }
+                }
+            }
+            catch (TooManyAttemptsException tme)
+            {
+                await context.PostAsync("Sorry, I wasn't able to understand your response. Please try asking for session information again.");
+                context.Wait(MessageReceived);
+            }
+            catch (Exception e)
+            {
+                await context.PostAsync("An error ocurred within TimeSlotChoiceAsync, please try again later.");
+                context.Wait(MessageReceived);
+
+            }
         }
 
         [LuisIntent("OrderPizza")]
